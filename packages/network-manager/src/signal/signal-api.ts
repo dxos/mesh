54f57cfffbe5ef3ed4707ec5acedcdc15124fd1e
Dmytro Maretskyi <maretskii@gmail.com>
2020-12-17 11:29:35 +0100

    Make signal API socket reconnect

--- packages/network-manager/src/signal/signal-api.ts
+++ packages/network-manager/src/signal/signal-api.ts
@@ -12,15 +12,35 @@ import { WebsocketRpc } from './websocket-rpc';
 
 const log = debug('dxos:network-manager:signal-api');
 
+const DEFAULT_RECONNECT_TIMEOUT = 1000;
 /**
  * Establishes a websocket connection to signal server and provides RPC methods.
  */
 export class SignalApi {
-  private _state = SignalApi.State.NOT_CONNECTED;
+  private _state = SignalApi.State.CONNECTING;
 
   private _lastError?: Error;
 
-  private _client: WebsocketRpc;
+  /**
+   * Number of milliseconds after which the connection will be attempted again in case of error.
+   */
+  private _reconnectAfter = DEFAULT_RECONNECT_TIMEOUT;
+
+  /**
+   * Timestamp of when the connection attempt was began.
+   */
+  private _connectionStarted = Date.now();
+
+  /**
+   * Timestamp of last state change.
+   */
+  private _lastStateChange = Date.now();
+
+  private _reconnectIntervalId?: NodeJS.Timeout;
+
+  private _client!: WebsocketRpc;
+
+  private _clientCleanup: (() => void)[] = [];
 
   readonly statusChanged = new Event<SignalApi.Status>();
 
@@ -36,7 +56,21 @@ export class SignalApi {
     private readonly _onOffer: (message: SignalApi.SignalMessage) => Promise<SignalApi.Answer>,
     private readonly _onSignal: (message: SignalApi.SignalMessage) => Promise<void>
   ) {
-    this._client = new WebsocketRpc(_host);
+    this._createClient();
+
+    this._setState(SignalApi.State.CONNECTING);
+  }
+
+  private _setState (newState: SignalApi.State) {
+    this._state = newState;
+    this._lastStateChange = Date.now();
+    log(`Signal state changed ${JSON.stringify(this.getStatus())}`);
+    this.statusChanged.emit(this.getStatus());
+  }
+
+  private _createClient() {
+    this._connectionStarted = Date.now();
+    this._client = new WebsocketRpc(this._host);
     this._client.addHandler('offer', (message: any) => this._onOffer({
       id: PublicKey.from(message.id),
       remoteId: PublicKey.from(message.remoteId),
@@ -52,38 +86,82 @@ export class SignalApi {
       data: msg.data
     }));
 
-    this._client.connected.on(() => this._setState(SignalApi.State.CONNECTED));
-    this._client.error.on(error => {
+    this._clientCleanup.push(this._client.connected.on(() => {
+      this._lastError = undefined;
+      this._reconnectAfter = DEFAULT_RECONNECT_TIMEOUT;
+      this._setState(SignalApi.State.CONNECTED)
+    }));
+    this._clientCleanup.push(this._client.error.on(error => {
+      if(this._state === SignalApi.State.CLOSED) {
+        return;
+      }
+
+      if(this._state === SignalApi.State.RE_CONNECTING) {
+        this._reconnectAfter *= 2;
+      }
+
       this._lastError = error;
-      this._setState(SignalApi.State.ERROR);
-
-      // TODO(marik-d): Reconnect.
-      console.error(error);
-    });
-    this._client.disconnected.on(() => {
       this._setState(SignalApi.State.DISCONNECTED);
-      // TODO(marik-d): Reconnect.
-    });
 
-    this._setState(SignalApi.State.CONNECTING);
+      this._reconnect();
+    }));
+    this._clientCleanup.push(this._client.disconnected.on(() => {
+      // This is also called in case of error, but we already have disconnected the socket on error, so no need to do anything here.
+      if(this._state !== SignalApi.State.CONNECTING && this._state !== SignalApi.State.RE_CONNECTING) {
+        return;
+      }
+
+      if(this._state === SignalApi.State.RE_CONNECTING) {
+        this._reconnectAfter *= 2;
+      }
+
+      this._setState(SignalApi.State.DISCONNECTED);
+
+      this._reconnect();
+    }));
   }
 
-  private _setState (newState: SignalApi.State) {
-    log(`Signal state changed ${this._host} ${newState}`);
-    this._state = newState;
-    this.statusChanged.emit(this.getStatus());
+  private _reconnect() {
+    if(this._reconnectIntervalId !== undefined) {
+      console.error('Signal api already reconnecting.')
+      return;
+    }
+
+    this._reconnectIntervalId = setTimeout(() => {
+      this._reconnectIntervalId = undefined;
+
+      this._clientCleanup.forEach(cb => cb());
+      this._clientCleanup = [];
+
+      // Close client if it wasn't already closed.
+      this._client.close().catch(() => {});
+
+      this._createClient();
+
+      this._setState(SignalApi.State.RE_CONNECTING);
+    }, this._reconnectAfter);
   }
 
   async close () {
+    this._clientCleanup.forEach(cb => cb());
+    this._clientCleanup = [];
+
+    if(this._reconnectIntervalId !== undefined) {
+      clearInterval(this._reconnectIntervalId);
+    }
+
     await this._client.close();
-    this._setState(SignalApi.State.DISCONNECTED);
+    this._setState(SignalApi.State.CLOSED);
   }
 
   getStatus (): SignalApi.Status {
     return {
       host: this._host,
       state: this._state,
-      error: this._lastError
+      error: this._lastError,
+      reconnectIn: this._reconnectAfter,
+      connectionStarted: this._connectionStarted,
+      lastStateChange: this._lastStateChange,
     };
   }
 
@@ -139,17 +217,29 @@ export class SignalApi {
 
 export namespace SignalApi {
   export enum State {
-    NOT_CONNECTED = 'NOT_CONNECTED',
+    /** Connection is being established. */
     CONNECTING = 'CONNECTING',
+
+    /** Connection is being re-established. */
+    RE_CONNECTING = 'RE_CONNECTING',
+
+    /** Connected. */
     CONNECTED = 'CONNECTED',
-    ERROR = 'ERROR',
+
+    /** Server terminated the connection. Socket will be reconnected. */
     DISCONNECTED = 'DISCONNECTED',
+
+    /** Socket was closed. */
+    CLOSED = 'CLOSED',
   }
 
   export interface Status {
     host: string,
     state: State,
     error?: Error
+    reconnectIn: number,
+    connectionStarted: number
+    lastStateChange: number
   }
 
   export interface CommandTrace {
